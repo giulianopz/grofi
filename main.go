dd4b737193a2801c9d679800f50fd407736c84fd
giuliano <panzironi.giuliano@gmail.com>
2023-06-30 00:41:42 +0200

    Open web if a pkg is selected

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
+/*
+refs:
+- https://man.archlinux.org/man/rofi-script.5.en
+- https://github.com/davatorium/rofi-scripts
+*/
+
 import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/go-resty/resty/v2"
+	"golang.org/x/exp/slices"
 	"golang.org/x/net/html"
 )
 
@@ -20,7 +28,11 @@ func main() {
 		searchQuery = "golang"
 	}
 
-	URI := fmt.Sprintf("https://pkg.go.dev/search?q=%s", searchQuery)
+	if isPkgPath(searchQuery) {
+		openPkgPage(searchQuery)
+	}
+
+	URI := fmt.Sprintf("https://pkg.go.dev/search?q=%s&limit=100&m=package#more-results", searchQuery)
 
 	client := resty.New()
 
@@ -41,8 +53,14 @@ func main() {
 			for _, att := range n.Attr {
 				if att.Key == "href" {
 					url := att.Val
-					if strings.HasPrefix(url, "/github.com") && strings.Count(url, "/") == 3 && !strings.Contains(url, "?") {
-						searchResults = append(searchResults, url)
+					if isPkgPath(url) {
+
+						url := trimSubPath(url)
+
+						if !slices.Contains(searchResults, url) {
+							// TODO: order by imports
+							searchResults = append(searchResults, url)
+						}
 					}
 				}
 			}
@@ -55,4 +73,34 @@ func main() {
 	f(doc)
 
 	fmt.Print(strings.Join(searchResults, "\n"))
+}
+
+func isPkgPath(url string) bool {
+	// TODO: include other git hosting services
+	return strings.HasPrefix(url, "/github.com") && !strings.Contains(url, "?")
+}
+
+func openPkgPage(pkgPath string) {
+	xdgOpen, err := exec.LookPath("xdg-open")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	args := []string{"https://" + pkgPath}
+
+	cmd := exec.Command(xdgOpen, args...)
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		os.Exit(0)
+	}
+}
+
+func trimSubPath(url string) string {
+	if strings.Count(url, "/") > 3 {
+		splitted := strings.Split(url, "/")
+		url = strings.Join(splitted[:4], "/")
+	}
+	return url
 }
